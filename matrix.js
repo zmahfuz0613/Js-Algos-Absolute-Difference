74f8359ecae100ad8b427effd299654c52954e46
Andre Pato <anpato1994@gmail.com>
2020-02-04 18:13:49 -0500

    initialize repo

---
+++ matrix.js
@@ -0,0 +1,16 @@
+/**
+ *  @function absoluteSumMatrix should calculate the absolute sum between both cross-axes in a grid matrix
+ *  @example Given a matrix:
+ * [
+ *   [ 1, 3, 4 ],
+ *   [ -2, -3, -5 ] ,
+ *   [ 6, 2, 5 ]
+ * ]
+ *  Find the absolute sum of each diagonal and return the total sum. The matrix will always be 3x3.
+ *  ex: 1 + -3 + 5 = 3, 4 + -3 + 6 = 7 return 3 + 7
+ *
+ */
+
+const absoluteSumMatrix = matrix => {}
+
+module.exports = absoluteSumMatrix
---
+++ tests/matrix.test.js
@@ -0,0 +1,34 @@
+const absoluteSumMatrix = require('../matrix')
+
+describe('Name of the group', () => {
+  describe.each([
+    [
+      [
+        [1, 3, 4],
+        [-2, -3, -5],
+        [6, 2, 5]
+      ],
+      10
+    ],
+    [
+      [
+        [6, 21, 1],
+        [4, -20, 1],
+        [9, 8, -10]
+      ],
+      34
+    ],
+    [
+      [
+        [400, 200, 300],
+        [120, -800, 213],
+        [-344, 20, -900]
+      ],
+      2144
+    ]
+  ])('absoulteSumMatrix(matrix)', (input, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(absoluteSumMatrix(input)).toBe(expected)
+    })
+  })
+})
